7581ddd55fc696e6fddcff57da90c6dfe25b1f3d
seokhh <119908334+seokhh@users.noreply.github.com>
2023-04-06 22:53:00 +0900

    Add files via upload

---
+++ AwtFirst.java
@@ -0,0 +1,17 @@
+package koreatech.sec2;
+import java.awt.*;
+public class AwtFirst {
+    public static void main(String[] args){
+        Frame f = new Frame("첫번째 AWT 프로그램");
+        f. setLayout(new FlowLayout());
+
+        Button b1 = new Button("확인");
+        Button b2 = new Button("취소");
+
+        f.add(b1);
+        f.add(b2);
+
+        f.setSize(300,200);
+        f.setVisible(true);
+    }
+}
---
+++ AwtSecond.java
@@ -0,0 +1,17 @@
+package koreatech.sec2;
+import java.awt.*;
+public class AwtSecond extends Frame{
+    AwtSecond(){
+        super("두번째 AWT 프로그램");
+        setLayout(new FlowLayout());
+        Button b1 = new Button("확인");
+        Button b2 = new Button("취소");
+        add(b1);
+        add(b2);
+    }
+    public static void main(String[] args){
+        AwtSecond as = new AwtSecond();
+        as.setSize(300,200);
+        as.setVisible(true);
+    }
+}
